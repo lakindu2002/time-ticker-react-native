7af5b6a581aac6068c5317b68e530f742c47e46f
lakindu hewawasam <lakindu2002@live.com>
2022-08-08 22:56:13 +0530

    adding the dynamic ticker with time format toggling

--- src/components/ticker.tsx
+++ src/components/ticker.tsx
@@ -1,27 +1,42 @@
 import format from "date-fns/format";
 import { FC, useEffect, useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
+
+const formatTime = (time: number, is12Hour: boolean) => {
+    if (is12Hour) {
+        return format(time, "hh:mm:ss a");
+    }
+    return format(time, "HH:mm:ss");
+}
 
 export const Ticker: FC = () => {
-    const [currentTime, setCurrentTime] = useState<string>('');
+    const [currentTime, setCurrentTime] = useState<number>(Date.now());
+    const [is12HourFormat, setIs12HourFormat] = useState<boolean>(false);
 
     useEffect(() => {
         const tick = () => {
             return setInterval(() => {
                 const timeInMs = Date.now();
-                const formattedTime = format(timeInMs, 'yyyy-MM-dd HH:mm:ss a');
-                setCurrentTime(formattedTime);
+                setCurrentTime(timeInMs);
             }, 1000);
         }
         const interval = tick();
         return () => clearInterval(interval);
     }, []);
 
+    const handleOnButtonPress = () => {
+        setIs12HourFormat((prevState) => !prevState);
+    }
+
     return (
         <View>
             <Text style={styles.textView}>
-                {currentTime}
+                {formatTime(currentTime, is12HourFormat)}
             </Text>
+            <Button
+                onPress={() => handleOnButtonPress()}
+                title={is12HourFormat ? "View In 24 Hour Format" : "View In 12 Hour Format"}
+            />
         </View>
     )
 };
@@ -30,5 +45,6 @@ const styles = StyleSheet.create({
     textView: {
         fontSize: 24,
         color: '#FF0000',
+        textAlign: 'center'
     }
 });
